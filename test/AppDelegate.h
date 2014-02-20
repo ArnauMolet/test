ed22777e5847398f3b77e09646c18dadced97356
ArnauMolet <arnau.molet@uvic.cat>
2014-02-20 18:09:07 +0100

    Hola

---
+++ test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by arnau.molet on 20/02/14.
+//  Copyright (c) 2014 arnau.molet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by arnau.molet on 20/02/14.
+//  Copyright (c) 2014 arnau.molet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
